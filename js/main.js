9d3870b250c278ff3fd17d3ae943b8d5f9ce391a
Tatiana Bernardo <tatianab@square-enix.com>
2017-02-08 15:26:44 +0000

    Remove jQuery code and add vanilla JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-if ( ! Modernizr.objectfit ) {
-  $('.thumbs').each(function () {
-    var $container = $(this),
-        imgUrl = $container.find('img').prop('src');
-    if (imgUrl) {
-      $container
-        .css('backgroundImage', 'url(' + imgUrl + ')')
-        .addClass('items-ie');
-    }
-  });
+function addClass(el, className) {
+	if (el.classList) el.classList.add(className);
+	else if (!hasClass(el, className)) el.className += ' ' + className;
+}
+
+var imgContainers, len;
+
+if (!Modernizr.objectfit) {
+	imgContainers = document.querySelectorAll('.item');
+	len = imgContainers.length;
+
+	for (var i=0; i<len; i++) {
+		var $container = imgContainers[i],
+				imgUrl = $container.querySelector('img').getAttribute('src');
+		if (imgUrl) {
+			$container.style.backgroundImage = 'url(' + imgUrl + ')';
+			addClass($container, 'item-ie');
+		}
+	}
 }
